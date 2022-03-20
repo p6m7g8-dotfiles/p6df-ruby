eb99927f94e1a530beb84fea7e939ee8c92a0fa4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-20 14:51:04 -0400

    chore: convets home::symlink to p6

--- init.zsh
+++ init.zsh
@@ -22,11 +22,11 @@ p6df::modules::ruby::deps() {
 ######################################################################
 p6df::modules::ruby::home::symlink() {
 
-  ln -fs $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-ruby/share/.gemrc .gemrc
-  ln -fs $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-ruby/share/.gemrc .riplrc
+  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-ruby/share/.gemrc" ".gemrc"
+  p6_file_symlink "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-ruby/share/.riplrc" ".riplrc"
 
-  mkdir -p $P6_DFZ_SRC_DIR/rbenv/rbenv/plugins
-  ln -fs $P6_DFZ_SRC_DIR/rbenv/ruby-build $P6_DFZ_SRC_DIR/rbenv/rbenv/plugins/ruby-build
+  p6_dir_mk "$P6_DFZ_SRC_DIR/rbenv/rbenv/plugins"
+  p6_file_symlink "$P6_DFZ_SRC_DIR/rbenv/ruby-build" "$P6_DFZ_SRC_DIR/rbenv/rbenv/plugins/ruby-build"
 }
 
 ######################################################################
